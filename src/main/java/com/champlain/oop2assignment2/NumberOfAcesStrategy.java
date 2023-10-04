8fa6d493e47ee6e7cc5378073682bfab770fb36f
Louis-Philippe <LE2253380@crc-lennox.qc.ca>
2023-10-04 16:50:29 -0400

    Creating NumberOfAcesStrategy Class

---
+++ src/main/java/com/champlain/oop2assignment2/NumberOfAcesStrategy.java
@@ -0,0 +1,11 @@
+package com.champlain.oop2assignment2;
+
+import java.util.List;
+
+public class NumberOfAcesStrategy implements ScoringStrategy {
+    @Override
+    public int calculateScore(List<Card> plist) {
+
+        return 0;
+    }
+}
