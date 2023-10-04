4e6ec304c87b7e7eb3ea288eea6f51893fcb7688
Louis-Philippe <LE2253380@crc-lennox.qc.ca>
2023-10-04 15:09:27 -0400

    Created SimpleCountStrategy Class

---
+++ src/main/java/com/champlain/oop2assignment2/SimpleCountStrategy.java
@@ -0,0 +1,13 @@
+package com.champlain.oop2assignment2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
+public class SimpleCountStrategy implements ScoringStrategy {
+    @Override
+    public int calculateScore(List<Card> plist) {
+
+        return 0;
+    }
+}
