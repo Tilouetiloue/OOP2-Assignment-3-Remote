716977e2e1ad97cf726dd26797f6979b9483ba43
Louis-Philippe <LE2253380@crc-lennox.qc.ca>
2023-10-04 16:39:38 -0400

    Removed useless import

--- src/main/java/com/champlain/oop2assignment2/SimpleCountStrategy.java
+++ src/main/java/com/champlain/oop2assignment2/SimpleCountStrategy.java
@@ -2,7 +2,7 @@ package com.champlain.oop2assignment2;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Iterator;
+
 
 public class SimpleCountStrategy implements ScoringStrategy {
     @Override
