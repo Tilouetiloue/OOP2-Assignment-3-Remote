b1c661f32b954a6d212cd8bfbbeff16a1ea07ef0
Louis-Philippe <LE2253380@crc-lennox.qc.ca>
2023-10-04 17:03:50 -0400

    Added code to Override calculateScore in NumberOfAcesStrategy

--- src/main/java/com/champlain/oop2assignment2/NumberOfAcesStrategy.java
+++ src/main/java/com/champlain/oop2assignment2/NumberOfAcesStrategy.java
@@ -5,7 +5,13 @@ import java.util.List;
 public class NumberOfAcesStrategy implements ScoringStrategy {
     @Override
     public int calculateScore(List<Card> plist) {
+        int count = 0;
 
-        return 0;
+        for (Card card : plist) {
+           if (card.getRank() == Rank.ACE) {
+               count++;
+           }
+        }
+        return count;
     }
 }
